e14be29110dcb55c0b7d600af43c45f71afd976f
unknown <adm_az_vsutar@eaholding.onmicrosoft.com>
2024-01-27 19:25:39 +0530

    d

--- code.js
+++ code.js
@@ -735,8 +735,505 @@ class TreeNode {
   
   // Call the getRightView function with the root of the tree
   console.log(getnumberofnodesinatree(treeForCountingNodes), "getnumberofnodesinatree");
+
+  //find the tree is valid or not means all the left value of a node should be lesser than root value and allthe right side value of root should be greaterthan root value
+
+
+  const dfs = function(root, min, max){
+    if(root.left){
+     if(!dfs(root.left,min,root.value)){
+      return false
+     }
+    }
+    if(root.right){
+     if(!dfs(root.right,root.value,max)){
+      return false
+     }
+    }
+    return true
+  }
+
+  const isTreeValid = (root)=>{
+    if(!root) return false
+    return dfs(root,-Infinity,Infinity)
+  }
+
+
+  //heap datastructure
+
+  class heapDatastructure{
+    constructor(comparator = (a,b)=>a>b){
+      this._heap = [];
+      this._comparator = comparator;
+
+
+    }
+
+    size(){
+      return this._heap.length;
+    }
+
+    peek(){
+      return this._heap[0]
+    }
+
+    isEmpty(){
+      return this._heap.length == 0
+    }
+
+    _parent(index){
+      return Math.floor((index - 1)/2);
+    }
+    _leftChild(index){
+      return index*2 + 1
+    }
+
+    _rightChild(index){
+      return index*2 + 2
+    }j
+    _swap(i,j){
+      [this._heap[i],this._heap[j]] = [this._heap[j],this._heap[i]]
+    }
+
+    _compare(i,j){
+      return this._comparator(this._heap[i],)
+    }
+    _shiftUp(){
+      let nodeIndex = this.size() - 1
+      while(nodeIndex > 0 && this._comparator(nodeIndex, this._parent(nodeIndex))){
+        this._swap(nodeIndex,this._parent(nodeIndex))
+        nodeIndex = this._parent(nodeIndex)
+      }
+    }
+
+    _push(val){
+      this._heap.push(val)
+      this._shiftUp()
+      return this.size()
+    }
+
+
+  }
+
+
+  const bfsdirectionstotraverseinfoursideinarray = [[-1,0],[0,1],[1,0],[0,-1]]
+  const dfsfortwodimensionalarray = (matrix,row,col, seen, values)=>{
+    if(row<0 || col < 0 || row > matrix.length -1 || col > matrix[0].length -1 || seen[row][col] == true) return false
+
+    values.push(matrix[row][col])
+
+    seen[row][col] = true
+    
+    for (let i = 0; i < bfsdirectionstotraverseinfoursideinarray.length; i++) {
+      const currentdir = bfsdirectionstotraverseinfoursideinarray[i]
+      dfsfortwodimensionalarray(matrix,row + currentdir[0], col + currentdir[1],seen,values)
+      
+    }
+     
+  }
+
+
+  const twodimensionalarraytraversal = (matrix)=>{
+      let seen = new Array(matrix.length).fill(0).map((item)=> new Array(matrix[0].length).fill(false))
+      let values = []
+      dfsfortwodimensionalarray(matrix,0,0,seen,values)
+      return values
+  }
+
+  console.log(twodimensionalarraytraversal([[1,2,3,4,5,6],[7,8,9,10,11,12],[13,14,15,16,17,18],[19,20,21,22,23,24]]), "twodimensionalarraytraversal")
+
+  const bfsdirectionstocountNumberOfIslands = [[-1,0],[0,1],[1,0],[0,-1]]
+  const countNumberOfIslands = function(matrix){
+    if(!matrix) return
+    let numberofislands = 0 
+    for (let row = 0; row < matrix.length; row++) {
+       for (let col = 0; col < matrix[0].length; col++) {
+         const currentElement = matrix[row][col]
+         if(currentElement === 1){
+          numberofislands++
+          matrix[row][col] = 0
+          const queue = []
+          queue.push([row,col])
+
+          while(queue.length){
+            const currentIslandPos = queue.shift()
+            const currentIslandRow = currentIslandPos[0]
+            const currentIslandCol = currentIslandPos[1]
+            for (let i = 0; i < bfsdirectionstocountNumberOfIslands.length; i++) {
+              const currentRowDir = bfsdirectionstocountNumberOfIslands[i];
+              let nextRow = currentIslandRow + currentRowDir[0]
+              let nextCol = currentIslandCol + currentRowDir[1]
+              if(nextRow < 0 || nextCol < 0 || nextRow >= matrix.length || nextCol >= matrix[0].length) continue
+              // else if(matrix[nextRow][nextCol] == 1){
+                if(matrix[nextRow][nextCol] == 1){
+                queue.push([nextRow,nextCol]);
+                matrix[nextRow][nextCol] = 0
+              }
+              
+            }
+          }
+         }
+        
+       }      
+    }
+    return numberofislands
+  }
+
+  console.log(countNumberOfIslands([
+    [1, 1, 1, 0, 0],
+    [1, 1, 1, 0, 1],
+    [0, 1, 0, 0, 1],
+    [0, 0, 0, 1, 1]
+  ]))
+
+
+
+  const rottenOranges = function(matrix){
+    const queue = []
+    let freshOranges = 0
+    for (let row = 0; row < matrix.length; row++) {
+       for (let col = 0; col < matrix[0].length; col++) {
+          if(matrix[row][col] == 2){
+            queue.push([row,col])
+          }
+
+          if(matrix[row][col] == 1){
+            freshOranges++
+          }
+         
+       }      
+    }
+  let currentQueueSize = queue.length
+  let minutes = 0
+
+     while(queue.length) {
+      if(currentQueueSize == 0){
+        minutes++
+        currentQueueSize = queue.length
+      }
+
+      let currentOrangeIndex = queue.shift()
+      currentQueueSize--
+      let currentOrangeIndexRow = currentOrangeIndex[0]
+      let currentOrangeIndexCol = currentOrangeIndex[1]
+      for (let i = 0; i < bfsdirectionstotraverseinfoursideinarray.length; i++) {
+        let currentDir = bfsdirectionstotraverseinfoursideinarray[i]
+        let nextRow = currentOrangeIndexRow + currentDir[0]
+        let nextCol = currentOrangeIndexCol + currentDir[1]
+        if(nextRow < 0 || nextCol < 0 || nextRow >= matrix.length || nextCol >= matrix[0].length) continue
+        if(matrix[nextRow][nextCol] == 1){
+          matrix[nextRow][nextCol] = 2
+          freshOranges--
+          queue.push([nextRow,nextCol])
+        }
+      }
+
+      
+     }
+
+     if(freshOranges > 0){
+      return -1
+     }
+
+     return minutes
+  }
+
+
+  console.log(rottenOranges([
+    [2, 1, 1, 1],
+    [1, 1, 0, 1],
+    [0, 1, 1, 1],
+    [1, 1, 1, 1]
+  ]),"rottenOranges")
+
+
+
+
+  const bfsingraphs = (graph)=>{
+    const queue = [0]
+    let values = []
+    let seen = {}
+    while(queue.length){
+      const vertex = queue.shift()
+      seen[vertex]= true
+      values.push(vertex)
+      const connections = graph[vertex]
+      for (let i = 0; i < connections.length; i++) {
+        const element = connections[i];
+        if(!seen[element]){
+          queue.push(element)
+        }
+        
+      }
+
+    }
+
+   return values
   
+  }
+
+  console.log(bfsingraphs([
+    [1,3],
+    [0],
+    [3,8],
+    [0,4,5,2],
+    [3,6],
+    [3],
+    [4,7],
+    [6],
+    [2]
+  ]),"bfsingraphs")
 
 
+  const dfsingraphs = (vertex,values,seen,graph)=>{
+   values.push(vertex)
+   seen[vertex] = true
+   const connection = graph[vertex]
+   for (let i = 0; i < connection.length; i++) {
+    const element = connection[i];
+    if(!seen[element]){
+      dfsingraphs(element,values,seen,graph)
+    }
+   }
 
+   return values
+  
+  }
+
+  console.log(dfsingraphs(0,[],{},[
+    [1,3],
+    [0],
+    [3,8],
+    [0,4,5,2],
+    [3,6],
+    [3],
+    [4,7],
+    [6],
+    [2]
+  ]),"dfsingraphs")
+
+  const dfsforinformtime = (currentId,adjlist,informTime)=>{
+    if(adjlist[currentId].length === 0) {
+      return 0;
+    }
+     let max = 0
+     let subordinates = adjlist[currentId]
+     for (let i = 0; i < subordinates.length; i++) {
+      const element = subordinates[i];
+      max = Math.max(max,dfsforinformtime(element,adjlist,informTime))
+      
+     }
+     return max + informTime[currentId]
+  }
+
+
+  const dfsofgraphswithmaxInformTimeOfEmployees = (n,headId,managers,informTime)=>{
+      let adjList = managers.map(()=>[])
+      for (let employee = 0; employee < n; employee++) {
+          let manager = managers[employee]
+          if(managers[employee] == -1) continue
+          adjList[manager].push(employee)       
+      }
+      return dfsforinformtime(headId,adjList,informTime)  
+  }
+
+  console.log(dfsofgraphswithmaxInformTimeOfEmployees(8,4,[2,2,4,6,-1,4,4,5],[0,0,4,0,7,3,6,0]),"dfsofgraphswithmaxInformTimeOfEmployees")
+
+
+  const iscoursecomplete = (n,prerequisist)=>{
+    let adjList = new Array(n).fill(0).map((item)=> [])
+    for (let i = 0; i < prerequisist.length; i++) {
+      const element = prerequisist[i];
+      adjList[element[1]].push(element[0])
+      
+    }
+
+    for (let v = 0; v < n; v++) {
+      const element = adjList[v];
+      let queue = []
+      let seen = {}
+      for (let i = 0; i < element.length; i++) {
+        const elementOFAdjList = element[i];
+        queue.push(elementOFAdjList)
+        
+      }
+
+      while(queue.length){
+        const currentElm = queue.shift()
+        seen[currentElm] = true
+        if(currentElm === v) return false;
+        const adjacent = adjList[currentElm];
+        for(let i = 0; i < adjacent.length; i++) {
+          const next = adjacent[i];
+          if(!seen[next]) {
+            queue.push(next);
+          }
+        }
+      }
+      
+    }
+
+    return true
+  }
+
+  console.log(iscoursecomplete(6,[[1, 0], [2, 1], [2, 5], [0, 3], [4, 3], [3, 5], [4, 5]]),"dfsofgraphswithmaxInformTimeOfEmployees")
+
+  //dijkstra algorithm
+
+  const times =[[1,2,9],[1,4,2],[2,5,1],[4,2,4],[4,5,6],[3,2,3],[5,3,7],[3,1,5]]
+  const k= 1
+  const N=5
+  // const networkTotalTime = (times,k,N)=>{
+  //    const distances = new Array(N).fill(Infinity);
+  //    const adjList = distances.map(()=>[])
+  //    distances[k-1] = 0
+  //    for (let i = 0; i < times.length; i++) {
+  //     const source = times[i][0]
+  //     const target = times[i][1]
+  //     const weight = times[i][2]
+
+  //     adjList[source-1].push([target,weight])
+      
+  //    }
+
+  //    const heap = new heapDatastructure((a,b)=> distances[a] < distances[b])
+
+  //    heap._heap.push(k-1)
+
+  //    while(!heap.isEmpty()){
+  //     const currentVertex = heap._heap.shift()
+  //     const adj = adjList[currentVertex]
+  //     for (let i = 0; i < adj.length; i++) {
+  //       const neighboringVertex = adj[i][0];
+  //       const neighboringVertexWeight = adj[i][1];
+
+  //       if(distances[currentVertex] + neighboringVertexWeight < distances[neighboringVertex]){
+  //         distances[neighboringVertex] = distances[currentVertex] + neighboringVertexWeight
+  //         heap._heap.push(neighboringVertex)
+  //       }
+        
+  //     }
+
+  //    }
+
+  // }
+
+  const networkTotalTime = (times,k,N) =>{
+    const distances = new Array(N).fill(Infinity)
+    const adjList = distances.map(()=> [])
+    distances[k-1]= 0
+    for (let i = 0; i < times.length; i++) {
+      const source = times[i][0];
+      const target = times[i][1];
+      const weight = times[i][2];
+      adjList[source-1].push([target,weight])
+    }
+    const heap = new heapDatastructure((a,b)=> distances[a] < distances[b])
+
+    heap._heap.push(k-1)
+
+    while(!heap.isEmpty()){
+      const currentvertex = heap._heap.pop()
+      const adjToCurrentVertex = adjList[currentvertex]
+      for (let i = 0; i < adjToCurrentVertex.length; i++) {
+        const neighboringVertexses = adjToCurrentVertex[i][0];
+        const neighboringVertexsesWeight = adjToCurrentVertex[i][1];
+        if(distances[currentvertex] + neighboringVertexsesWeight < distances[neighboringVertexses]){
+          distances[neighboringVertexses] = distances[currentvertex] + neighboringVertexsesWeight
+          heap._heap.push(currentvertex)
+        }
+        
+      }
+    }
+    const ans = Math.max(...distances)
+    return ans === Infinity ? -1 : ans
+  }
+
+  
+  console.log(networkTotalTime(times, k,N),"networkTotalTime")
+
+
+  const minCost = (i, costArray)=>{
+    if(i == 0 || i==1) return costArray[i]
+    return costArray[i]+ Math.min(minCost(i-1,costArray),minCost(i-2,costArray))
+  }
+
+  const minCostClimbing = (costArray)=>{
+     let n = costArray.length
+     return Math.min(minCost(n-1,costArray),minCost(n-2,costArray))
+  }
+
+  console.log(minCostClimbing([20,15,30,6],"minCostClimbing"));
+
+  const optimizedminCost = (i, costArray,dynamicProgrammingOptimization)=>{
+    if(i == 0 || i==1) return costArray[i]
+    if(dynamicProgrammingOptimization[i]) return dynamicProgrammingOptimization[i]
+    dynamicProgrammingOptimization[i] = costArray[i]+ Math.min(optimizedminCost(i-1,costArray,dynamicProgrammingOptimization),optimizedminCost(i-2,costArray,dynamicProgrammingOptimization))
+    return dynamicProgrammingOptimization[i]
+  }
+
+  const optimizedminCostClimbing = (costArray)=>{
+     let n = costArray.length
+     let dynamicProgrammingOptimization = []
+     return Math.min(optimizedminCost(n-1,costArray,dynamicProgrammingOptimization),optimizedminCost(n-2,costArray,dynamicProgrammingOptimization))
+  }
+
+  console.log(optimizedminCostClimbing([20,15,30,6],"optimizedminCostClimbing"));
+
+  const optimizedBottomUpminCostClimbing = (costArray)=>{
+    let n = costArray.length
+    let dynamicProgrammingOptimization = []
+    for (let i = 0; i < n; i++) {
+      if(i<2){
+        dynamicProgrammingOptimization[i] = costArray[i]
+      }else{
+        dynamicProgrammingOptimization[i] = costArray[i] + Math.min(dynamicProgrammingOptimization[i-1],dynamicProgrammingOptimization[i-2])
+      }
+    }
+    return Math.min(dynamicProgrammingOptimization[n-1],dynamicProgrammingOptimization[n-2])
+ }
+
+ console.log(optimizedBottomUpminCostClimbing([20,15,30,6]),"optimizedBottomUpminCostClimbing");
+
+ // in a chess how much probability of a knight to step out of chess board
+
+//  [
+//   [0,1,2,3,4,5],
+//   [0,1,2,3,4,5],
+//   [0,1,2,3,4,5],
+//   [0,1,2,3,4,5],
+//   [0,1,2,3,4,5],
+//   [0,1,2,3,4,5]
+// ]
+
+const directions = [
+  [-2,-1],
+  [-1,2],
+  [1,-2],
+  [2,-1],
+  [2,1],
+  [1,2],
+  [-1,2],
+  [-2,1],
+]
+
+ const knightProbability = (n,k,r,c)=>{
+  if(r<0 || c< 0 || n < 0 || r>n || c>n){
+    return 0
+  }
+  if(k === 0){
+    return 1
+  }
+
+  let res = 0
+  for (let index = 0; index < directions.length; index++) {
+  let current = directions[index]
+    res += knightProbability(n,k-1,r+current[0],c+current[1])/8
+    
+  }
+
+  return res
+ }
+
+ console.log(knightProbability(6,3,2,2),"knightProbability");
   
